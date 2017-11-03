6d58fea43968e845523da683d88e231c78d053b0
Theerapat <theerapat@isc-intl.com>
2017-11-03 09:54:07 +0100

    Added user interface

---
+++ app/src/main/java/com/theerapat/calculatorfloating/FloatingViewService.java
@@ -0,0 +1,57 @@
+package com.theerapat.calculatorfloating;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Created by theerapat on 11/3/2017.
+ */
+
+public class FloatingViewService extends Service {
+    private WindowManager mWindowManager;
+    private View mFloatingView;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        includeSetupTeardown();
+    }
+
+    private void includeSetupTeardown(){
+        setComponent();
+        setupWindowManager();
+    }
+
+    private void setComponent(){
+        mFloatingView = LayoutInflater.from(this).inflate(R.layout.layout_floating_widget, null);
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+    }
+
+    /*Add the view to the window.
+    Specify the view position by initial view will be added to top-left corner*/
+    private void setupWindowManager(){
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.LEFT;        //Initially view will be added to top-left corner
+        params.x = 0;
+        params.y = 100;
+        mWindowManager.addView(mFloatingView, params);
+    }
+}
--- app/src/main/java/com/theerapat/calculatorfloating/MainActivity.java
+++ app/src/main/java/com/theerapat/calculatorfloating/MainActivity.java
@@ -1,13 +1,72 @@
 package com.theerapat.calculatorfloating;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        render();
+    }
+
+    /*Check if the application has draw over other apps permission or not?
+    This permission is by default available for API<23. But for API > 23
+    you have to ask for the permission in runtime.*/
+    private void render(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this))
+            requestPermission();
+        else
+            initializeView();
+    }
+
+    /*If the draw over permission is not available open the settings screen
+    to grant the permission.*/
+    private void requestPermission(){
+        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+        startActivityForResult(intent, CODE_DRAW_OVER_OTHER_APP_PERMISSION);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CODE_DRAW_OVER_OTHER_APP_PERMISSION)
+            proceed(resultCode);
+        else
+            super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    //Check if the permission is granted or not.
+    private void proceed(int resultCode){
+        if (resultCode == RESULT_OK)
+            initializeView();
+        else { //Permission is not available
+            Toast.makeText(this,
+                    "Draw over other app permission not available. Closing the application",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    /**
+     * Set and initialize the view elements.
+     */
+    private void initializeView() {
+        findViewById(R.id.button_open_floating).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, FloatingViewService.class));
+                finish();
+            }
+        });
     }
 }
